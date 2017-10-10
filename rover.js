75b9da5e9343946cbf0c71e66232968edee49b90
Dafne <olca.dafne@gmail.com>
2017-10-10 18:08:27 +0200

    Update rover.js

--- rover.js
+++ rover.js
@@ -1,72 +1,26 @@
-console.log('Welcome to project Mars Rover');
-console.log('The rover is currently at position'+myRover.position);
-console.log('Please enter the commands you want the rover to move: ');
-userInput= //user input given by commands
-
-
+// VARIABLES AND FUNCTIONS
 var myRover = {
   position: [0,0],
-  direction: 'N'
-};
-
-var gridLimit=[[0,1,2,3,4,5,6,7,8,9], [0,1,2,3,4,5,6,7,8,9]];
-
-
-
-for (let numberOfCommands=0; numberOfCommands<userInput.length; numberOfCommands++){
-
-  if (userInput(numberOfCommands[])==="f"){
-    function goForward(myRover);
-    console.log("cool story bro");
-  } else if ((userInput(numberOfCommands[])==="b"){
-    function goBackward(myRover);
-  }
-  else if ((userInput(numberOfCommands[])==="l"){
-    console.log("Turned Left");
-
-    if direction==="W" {
-      direction='S'
-    }
-    else if direction==="S" {
-      direction='E'
-    }
-    else if direction==="E" {
-      direction='N'
-    }
-    else if direction==="N" {
-      direction='S'
-    }
-    else {
-      print "That command does not exist";
-    }
-
-  }
-  else if (userInput==="r"){
-    console.log("Turned Left");
-
-    if direction==="W" {
-      direction='N'
-    }
-    else if direction==="N" {
-      direction='E'
-    }
-    else if direction==="E" {
-      direction='S'
-    }
-    else if direction==="S" {
-      direction='E'
-    }
-    else {
-      print "That command does not exist";
-    }
-  }
+  direction: 'N',
+  grid: [0,0]
 }
+
+
+function gridLimit(myRover) {
+  if (myRover.direction==="N" && userInput ==="f") {
+    while (myRover.grid[0]!==10){
+      myRover.grid[0]++;
+    }
+
+  }
+
 }
+
+
 function goForward(myRover) {
   switch(myRover.direction) {
     case 'N':
       myRover.position[0]++
-      console.log("rover has moved.")
       break;
     case 'E':
       myRover.position[1]++
@@ -77,9 +31,10 @@ function goForward(myRover) {
     case 'W':
       myRover.position[1]--
       break;
-  };
+  }
+}
 
-  function goBackward(myRover) {
+function goBackward(myRover) {
     switch(myRover.direction) {
       case 'N':
         myRover.position[0]--
@@ -93,10 +48,77 @@ function goForward(myRover) {
       case 'W':
         myRover.position[1]++
         break;
-    };
-
-
-  console.log("New Rover Position: [" + myRover.position[0] + ", " + myRover.position[1] + "]")
+    }
 }
 
-goForward(myRover);
+
+
+//INTRODUCTION AND USERPROMPT
+console.log("Welcome to project Mars Rover");
+console.log("The rover is currently at starting position "+myRover.position+", direction: "+myRover.direction);
+userInput = prompt("Please enter the commands you want the rover to move: ");
+console.log("You entered: "+userInput);
+
+
+//FUNCTION IS IMPLEMENTED WITH LOOPS
+for (let numberOfCommands=0; numberOfCommands<userInput.length; numberOfCommands++){
+  let letter = userInput.charAt(numberOfCommands);
+
+
+  if (letter==="f"){
+    gridLimit(myRover)
+    goForward(myRover)
+
+    }
+
+
+  else if (letter==="b"){
+    goBackward(myRover)
+    }
+
+
+  else if (letter==="l"){   //CHANGES DIRECTION TO LEFT
+    if (myRover.direction==="W") {
+      myRover.direction='S'
+    }
+    else if (myRover.direction==="N") {
+      myRover.direction='W'
+    }
+    else if (myRover.direction==="S") {
+      myRover.direction='E'
+    }
+    else if (myRover.direction==="E") {
+      myRover.direction='N'
+    }
+    else {
+      console.log( "That command does not exist")
+    }
+  }
+
+  else if (letter==="r"){   //CHANGES DIRECTION TO RIGHT
+    if (myRover.direction==="W") {
+      myRover.direction='N'
+    }
+    else if (myRover.direction==="N") {
+      myRover.direction='E'
+    }
+    else if (myRover.direction==="S") {
+      myRover.direction='W'
+    }
+    else if (myRover.direction==="E") {
+      myRover.direction='S'
+    }
+    else {
+      console.log( "That command does not exist")
+    }
+  }
+
+  else {
+    console.log('Invalid command.')
+  }
+}
+
+
+//FINAL STATEMENTS
+console.log("The new position is: "+myRover.position)
+console.log("New direction: "+myRover.direction)
